d7bb91be9274c8c54564da942b644d0bce5ca441
LuizFBastiao <lbastiao@alunos.utfpr.edu.br>
2025-04-30 10:30:47 -0300

    feito um novo arquivo para a tela do botao pulsante e colocado na pasta pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'pages/TelaBotaoPulsante.dart';
 import 'widgets/BotaoPulsante.dart';
+'
 
 void main() => runApp(const MyApp());
 
@@ -11,45 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Botão Pulsante',
       home: const TelaBotaoPulsante(),
-    );
-  }
-}
-
-class TelaBotaoPulsante extends StatelessWidget {
-  const TelaBotaoPulsante({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blueGrey,
-        title: const Text('Botão Pulsante.'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 300),
-            BotaoPulsante(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Botão Pulsante Pressionado!')),
-                );
-              },
-            ),
-            SizedBox(height: 400),
-            const Text(
-              'Luiz Felipe Bastião',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            const Text(
-              'João Victor Pires Novais',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/pages/TelaBotaoPulsante.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TelaBotaoPulsante extends StatelessWidget {
+  const TelaBotaoPulsante({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Botão Pulsante.'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 300),
+            BotaoPulsante(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Botão Pulsante Pressionado!')),
+                );
+              },
+            ),
+            SizedBox(height: 400),
+            const Text(
+              'Luiz Felipe Bastião',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            const Text(
+              'João Victor Pires Novais',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
