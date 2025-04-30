90621c280a5c9e78b6f26ecfbe684b8ea0ec2ccc
LuizFBastiao <lbastiao@alunos.utfpr.edu.br>
2025-04-30 10:32:00 -0300

    correções sintaxe

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/TelaBotaoPulsante.dart';
 import 'widgets/BotaoPulsante.dart';
-'
 
 void main() => runApp(const MyApp());
 
--- lib/pages/TelaBotaoPulsante.dart
+++ lib/pages/TelaBotaoPulsante.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/BotaoPulsante.dart';
 
 class TelaBotaoPulsante extends StatelessWidget {
   const TelaBotaoPulsante({super.key});
